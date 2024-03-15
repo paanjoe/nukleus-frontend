d2ee8fb1ea65ea47c5e31dd2f30d608b11923966
PaanJoe <farhan.gunz@gmail.com>
2024-03-16 00:48:47 +0800

    add repo link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import LoginMain from "./LoginMain";
 
 export default function Header() {
@@ -17,6 +18,36 @@ export default function Header() {
           The backend is on free-tier plan, so it might take up to 50 seconds to
           start the back-end service for the first time.
         </span>
+        <br />
+        <br />
+        <a
+          href="https://github.com/paanjoe/nukleus-frontend"
+          target="_blank"
+          className="bg-red-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 dark:hover:underline hover:underline"
+        >
+          Front-End Repo Click Here{" "}
+        </a>
+        <a
+          href="https://github.com/paanjoe/nukleus-backend"
+          target="_blank"
+          className="bg-pink-100 text-pink-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300 dark:hover:underline hover:underline"
+        >
+          Back-End Repo Click Here{" "}
+        </a>
+        <a
+          href="https://github.com/paanjoe/nukleus-domain"
+          target="_blank"
+          className="bg-purple-100 text-purple-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-purple-900 dark:text-purple-300 dark:hover:underline hover:underline"
+        >
+          Domain Repo Click Here{" "}
+        </a>
+        <a
+          href="https://www.figma.com/file/AE6vCE7lwxDaMI32c0nVWk/%5BFarhan%5D-GovTech-Assessment-Brainstorm-Board?type=whiteboard&t=ewtZZiMzR75Sc1k7-1"
+          target="_blank"
+          className="bg-yellow-100 text-yellow-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300 dark:hover:underline hover:underline"
+        >
+          Figma Documentation{" "}
+        </a>
       </p>
       <LoginMain />
     </div>
